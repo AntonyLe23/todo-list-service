6b7b210e61054193553eb84cfcf309abeb4c20f7
Phat.Le <phat.le@sevencircles.com>
2024-06-21 20:25:42 +0200

    fix: [master] service name

--- src/main/java/com/lifung/todolistservice/TodoListServiceApplication.java
+++ src/main/java/com/lifung/todolistservice/TodoListServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodolistserviceApplication {
+public class TodoListServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodolistserviceApplication.class, args);
+		SpringApplication.run(TodoListServiceApplication.class, args);
 	}
 
 }
