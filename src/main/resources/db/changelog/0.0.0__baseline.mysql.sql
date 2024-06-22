31daacd017879dbb77e2932a61925baed9cfb2a0
Phat.Le <phat.le@sevencircles.com>
2024-06-22 07:15:46 +0200

    feat: [master] config mysql

---
+++ src/main/resources/db/changelog/0.0.0__baseline.mysql.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users
+(
+    id       bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    username varchar(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE todo_items
+(
+    id bigint AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    todo varchar(100),
+    description text,
+    completed bool default 0,
+    user_id bigint,
+    CONSTRAINT `fk_todo_item_user` FOREIGN KEY (user_id) REFERENCES users(id)
+)
